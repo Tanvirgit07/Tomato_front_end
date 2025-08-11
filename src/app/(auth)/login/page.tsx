07ac9226a8c330ec3ac7eec043ff3d32fc125313
Tanvirgit07 <ahmmedtanvir9090@gmail.com>
2025-08-11 22:52:28 +0600

    new add

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SignInFrom } from './_components/SignInFrom'
+
+function page() {
+  return (
+    <div>
+        <SignInFrom />
+    </div>
+  )
+}
+
+export default page
