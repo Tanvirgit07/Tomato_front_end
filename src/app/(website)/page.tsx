2fd461b3631d0cec5dff84c7c2f9fcf422f14eeb
Tanvirgit07 <ahmmedtanvir9090@gmail.com>
2025-08-12 00:29:07 +0600

    new add

---
+++ src/app/(website)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeHero from './_components/HomeHero'
+
+function page() {
+  return (
+    <div>
+        <HomeHero />
+    </div>
+  )
+}
+
+export default page
