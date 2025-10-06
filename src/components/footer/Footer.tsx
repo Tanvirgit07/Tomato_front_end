7a8743fde8a60ed2df484076d0dacaefa7e90cd5
Tanvirgit07 <ahmmedtanvir9090@gmail.com>
2025-10-07 01:56:12 +0600

    add

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -11,6 +11,10 @@ const Footer = () => {
     console.log('Newsletter subscription submitted');
   };
 
+
+
+  
+
   return (
     <footer className="bg-gray-900 text-gray-200 py-12 md:py-16 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
